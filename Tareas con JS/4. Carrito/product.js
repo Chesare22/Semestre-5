49206acae53e089036a76d3e3a25e7f334fc0315
Cesar <chesarglez429@gmail.com>
2019-10-12 11:54:46 -0500

    pagina1 usa JSON para la cookie

---
+++ Tareas con JS/4. Carrito/product.js
@@ -0,0 +1,23 @@
+class Product {
+  constructor(name, price, shipping, ammount) {
+    this.name = name
+    this.price = price
+    this.shipping = shipping
+    this.ammount = ammount >= 0 ? ammount : 1
+  }
+
+  increaseAmmount() {
+    this.ammount++
+  }
+  decreaseAmmount() {
+    this.ammount -= 1
+  }
+}
+
+function objectsToProducts(objects) {
+  const products = []
+  objects.forEach(({ name, price, shipping, ammount }) => {
+    products.push(new Product(name, price, shipping, ammount))
+  });
+  return products
+}
