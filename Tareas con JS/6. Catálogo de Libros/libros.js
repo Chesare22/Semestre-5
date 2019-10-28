780e4f154e1698b598323cebf0c42a1f0daf37b1
César <Chesare22@github.com>
2019-10-27 22:37:36 -0600

    La página de libros ahora guarda los datos en LocalStorage

--- Tareas con JS/6. Catálogo de Libros/libros.js
+++ Tareas con JS/6. Catálogo de Libros/libros.js
@@ -1,63 +1,49 @@
-/* exported agregarLibro imprimirLibros */
+const libros = window.localStorage.books ? JSON.parse(window.localStorage.books) : []
 
-const libros = []
-
-function Libro (titulo, autor, editorial, tema) {
-  this.titulo = titulo.toUpperCase()
+function Libro (titulo, autor, editorial) {
+  this.titulo = titulo
   this.autor = autor
   this.editorial = editorial
-  this.tema = tema.toLowerCase()
 
-  this.escribirLibro = function escribirLibro () {
+  this.escribirLibro = () => {
     alert(`El libro ${this.titulo} de ${this.autor} trata sobre ${this.tema}`)
   }
 
   this.urlParams = function urlParams () {
     return `titulo=${this.titulo}
       &autor=${this.autor}
-      &editorial=${this.editorial}
-      &tema=${this.tema}`
+      &editorial=${this.editorial}`
   }
 }
 
-function agregarLibro ({ titulo, autor, editorial, tema }) {
-  if (!titulo.value) {
-    alert('Debes escribir el título')
-    titulo.focus()
-    return false
-  }
-  if (!autor.value) {
-    alert('Debes escribir el autor')
-    autor.focus()
-    return false
-  }
-  if (!editorial.value) {
-    alert('Debes escribir la editorial')
-    editorial.focus()
-    return false
-  }
-  if (!tema.value) {
-    alert('Debes escribir el tema')
-    tema.focus()
-    return false
-  }
-
-  libros.push(new Libro(titulo.value, autor.value, editorial.value, tema.value))
-
-  titulo.value = ''
-  autor.value = ''
-  editorial.value = ''
-  tema.value = ''
-
-  return true
-}
-
-function imprimirLibros () {
-  let offset = 5
-  libros.forEach(libro => {
-    offset += 15
-    window.open(`new-window.html?${libro.urlParams()}`,
-      '',
-      `width=270,height=240,left=${offset},top=${offset}`)
+window.onload = () => {
+  const forma = document.getElementById('forma')
+  forma.agregar.addEventListener('click', () => {
+    const { titulo, autor, editorial } = forma
+  
+    if (!titulo.value) {
+      alert('Debes escribir el título')
+      titulo.focus()
+      return false
+    }
+    if (!autor.value) {
+      alert('Debes escribir el autor')
+      autor.focus()
+      return false
+    }
+    if (!editorial.value) {
+      alert('Debes escribir la editorial')
+      editorial.focus()
+      return false
+    }
+  
+    libros.push(new Libro(titulo.value, autor.value, editorial.value))
+  
+    titulo.value = ''
+    autor.value = ''
+    editorial.value = ''
+  
+    window.localStorage.setItem('books', JSON.stringify(libros))
+    // window.open('catalog_table.html', 'catalog_table')
   })
 }
