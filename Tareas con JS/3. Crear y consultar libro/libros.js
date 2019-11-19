d5e8ca1495965780cfef77eb1191f7e4e3aeff2a
Chesare22 <Chesare22@github.com>
2019-11-19 02:26:33 -0600

    Códigos viejos ahora cumplen las reglas de ESLint.

--- Tareas con JS/3. Crear y consultar libro/libros.js
+++ Tareas con JS/3. Crear y consultar libro/libros.js
@@ -1,16 +1,18 @@
+/* exported agregarLibro imprimirLibros */
+
 const libros = []
 
-function Libro(titulo, autor, editorial, tema) {
+function Libro (titulo, autor, editorial, tema) {
   this.titulo = titulo.toUpperCase()
   this.autor = autor
   this.editorial = editorial
   this.tema = tema.toLowerCase()
 
-  this.escribirLibro = function escribirLibro() {
-    alert("El libro " + this.titulo + " de " + this.autor + " trata sobre " + this.tema)
+  this.escribirLibro = function escribirLibro () {
+    alert(`El libro ${this.titulo} de ${this.autor} trata sobre ${this.tema}`)
   }
 
-  this.urlParams = function urlParams() {
+  this.urlParams = function urlParams () {
     return `titulo=${this.titulo}
       &autor=${this.autor}
       &editorial=${this.editorial}
@@ -18,42 +20,41 @@ function Libro(titulo, autor, editorial, tema) {
   }
 }
 
-function agregarLibro({titulo, autor, editorial, tema}) {
-
+function agregarLibro ({ titulo, autor, editorial, tema }) {
   if (!titulo.value) {
-    alert("Debes escribir el título")
+    alert('Debes escribir el título')
     titulo.focus()
     return false
   }
   if (!autor.value) {
-    alert("Debes escribir el autor")
+    alert('Debes escribir el autor')
     autor.focus()
     return false
   }
   if (!editorial.value) {
-    alert("Debes escribir la editorial")
+    alert('Debes escribir la editorial')
     editorial.focus()
     return false
   }
   if (!tema.value) {
-    alert("Debes escribir el tema")
+    alert('Debes escribir el tema')
     tema.focus()
     return false
   }
 
   libros.push(new Libro(titulo.value, autor.value, editorial.value, tema.value))
 
-  titulo.value = ""
-  autor.value = ""
-  editorial.value = ""
-  tema.value = ""
+  titulo.value = ''
+  autor.value = ''
+  editorial.value = ''
+  tema.value = ''
 
   return true
 }
 
-function imprimirLibros() {
+function imprimirLibros () {
   let offset = 5
-  libros.forEach((libro) => {
+  libros.forEach(libro => {
     offset += 15
     window.open(`new-window.html?${libro.urlParams()}`,
       '',
